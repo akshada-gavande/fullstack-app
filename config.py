c49dc81b6c7dda4e628aecd4042a1843c58001ef
AkshadaGavande <40367604+AkshadaGavande@users.noreply.github.com>
2021-01-25 15:48:56 +0530

    add files

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "emp.cybd4db61dcn.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "adminadmin"
+customdb = "emp"
+custombucket = "adddemployee"
+customregion = "us-east-1"
